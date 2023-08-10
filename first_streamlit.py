266a6b2b04daae5de3e6d74e5aaca0b1c97360f8
itsammara <57931272+itsammara@users.noreply.github.com>
2023-08-10 14:34:17 +0530

    Update first_streamlit.py
    Header and Title Added

--- first_streamlit.py
+++ first_streamlit.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('Ammar First App')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry')
+streamlit.text('Khawsey')
+streamlit.text('Mitthi ki BHaji')
