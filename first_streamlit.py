b000c4fbac00ebe8962ae193d8b8333b5aefa8e0
itsammara <57931272+itsammara@users.noreply.github.com>
2023-08-10 14:50:10 +0530

    Update first_streamlit.py
    Added Pandas

--- first_streamlit.py
+++ first_streamlit.py
@@ -5,3 +5,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry')
 streamlit.text('Khawsey')
 streamlit.text('Mitthi ki BHaji')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
